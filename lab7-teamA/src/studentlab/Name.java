25f1c5b08a98396fb3865d046beba55a57539779
abdullahadeeb <abdullahadeeb@me4233-pc19.labs.sce.carleton.ca>
2013-10-23 16:45:44 -0400

    done course

---
+++ lab7-teamA/src/studentlab/Name.java
@@ -0,0 +1,9 @@
+public class Name
+{
+   private String first, last;
+   public Name(String first, String last) 
+   {
+   }
+   public String toString() { return null; }
+   public boolean equals(Object o) { return false; }
+}
